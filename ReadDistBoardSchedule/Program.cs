5a4d10319a33cad08be6ab3bd3ac9fdfb436a2eb
stephentetley <stephen.tetley@gmail.com>
2026-05-18 21:59:40 +0100

    Initial work on new util ReadDistBoardSchedule

---
+++ ReadDistBoardSchedule/Program.cs
@@ -0,0 +1,101 @@
+﻿// Copyright 2026 Stephen Tetley
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+using System.CommandLine;
+using System.Text.Json;
+using ClosedXML.Excel;
+using Microsoft.Extensions.FileSystemGlobbing;
+
+
+namespace SptUtils.ReadDistBoardSchedule
+{
+
+    class Program
+    {
+        static int Main(string[] args) 
+        {
+
+            RootCommand rootCommand = new("ReadDistBoardSchedule") 
+            {
+                new Option<string>("--schedule_folder")
+                {
+                    Description = "Path to folder with Dist Board schedule xlsx files",
+                    Required = true
+                },              
+            };
+            rootCommand.SetAction(parseResult =>
+            {
+                string? dbScheduleFolder = parseResult.GetValue<string>("--schedule_folder");
+                return ProcessWorkbooks(dbScheduleFolder);
+            });
+            ParseResult parseResult = rootCommand.Parse(args);
+            return parseResult.Invoke();
+        }
+
+        private static int ProcessWorkbooks(string? dbScheduleFolder)
+        {
+            if (Directory.Exists(dbScheduleFolder)) {
+                int ans = 0;
+                Matcher matcher = new();
+                matcher.AddInclude("*.xlsx");
+                Console.WriteLine("Source: " + dbScheduleFolder);
+
+                var matchingFiles = matcher.GetResultsInFullPath(dbScheduleFolder);
+                foreach (var dbSchedule in matchingFiles)
+                {
+                    var res = ProcessWorkbook(dbSchedule);
+                    ans += res;
+                }
+                return (ans > 1) ? 1 : 0;
+            } 
+            else
+            {
+                return 1;
+            }
+
+        }
+
+        private static int ProcessWorkbook(string? dbSchedule)
+        {
+            if (File.Exists(dbSchedule))
+            {
+                var outputJsonFile = Path.ChangeExtension(dbSchedule, ".json");
+                var options = new JsonWriterOptions { Indented = true };
+                using var workbook = new XLWorkbook(dbSchedule);
+                using var stream = File.Create(outputJsonFile);
+                using var writer = new Utf8JsonWriter(stream, options);
+                var fileName = Path.GetFileName(dbSchedule);
+                writer.WriteStartArray();
+                foreach (var worksheet in workbook.Worksheets)
+                {
+                    var reader = new ReadSchedule(fileName, worksheet);
+
+                    if (reader.IsTestSheet())
+                    {
+                        Console.WriteLine("Sheet: " + worksheet.Name + " " + reader.IsTestSheet());
+                        var tests = reader.ParseTestSheet();
+                        foreach(var test in tests) test.WriteJson(writer);
+                    }
+                }
+                writer.WriteEndArray();
+                Console.WriteLine("Wrote: " + outputJsonFile);
+                return 0;
+            } else {
+                Console.WriteLine("Invalid arguments");
+                return 1;
+            }
+        }
+    }
+}
---
+++ ReadDistBoardSchedule/src/ReadDistBoardSchedule/ReadSchedule.cs
@@ -0,0 +1,121 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+using ClosedXML.Excel;
+
+namespace SptUtils.ReadDistBoardSchedule 
+{
+
+    public class ReadSchedule(string xlsxName, IXLWorksheet ws)
+    {
+        IXLWorksheet sheet = ws;
+        string fileName = xlsxName;
+
+        public bool IsTestSheet()
+        {
+            var value = sheet.Cell("F1").GetString();
+            return (value == "TEST SHEET");
+
+        }
+
+        public List<AsFittedCircuit> ParseTestSheet()
+        {
+            var circuits = new List<AsFittedCircuit>();
+            string[] columns = { "C", "D", "E", "F", "G", "H", "I", "J", "K"};
+            var header = ReadHeader();
+            foreach (var column in columns) {
+                var test = readCircuit(column, header);
+                if (test != null) circuits.Add(test);
+            }
+            return circuits;
+        }
+
+        private TestHeader ReadHeader()
+        {
+            var tabName = sheet.Name;
+            var siteName = sheet.Cell("B3").GetString();
+            var dbOrPanelNumber = sheet.Cell("E3").GetString();
+            var testDate = sheet.Cell("J3").GetString();
+            var sheetNumber = sheet.Cell("K3").GetString();
+            var aibRef = sheet.Cell("B5").GetString();
+            var tpOrSp = sheet.Cell("E5").GetString();
+            var location = sheet.Cell("F5").GetString();
+            var dbOrPanelIncomerDetails = sheet.Cell("B7").GetString();
+
+            return new TestHeader
+            (
+                FileName: fileName, 
+                TabName: tabName, 
+                SiteName: siteName, 
+                DbOrPanelNumber: dbOrPanelNumber, 
+                TestDate: testDate, 
+                SheetNumber: sheetNumber, 
+                AibRef: aibRef, 
+                TpOrSp: tpOrSp,
+                Location: location,
+                DbOrPanelIncomerDetails: dbOrPanelIncomerDetails
+            );
+        }
+
+        // columns ["C" .. "K"]
+        private AsFittedCircuit? readCircuit(string col, TestHeader header)
+        {
+            var cableNum = sheet.Cell(10, col).GetString();
+            var fedFrom = sheet.Cell(11, col).GetString();
+            var circuitRefAndPhase = sheet.Cell(12, col).GetString();
+            
+            if (cableNum == "" && fedFrom == "" && circuitRefAndPhase == "")
+            {
+                return null;
+            } 
+            else
+            {
+                return new AsFittedCircuit
+                (
+                    FileName: header.FileName, 
+                    TabName: header.TabName, 
+                    SiteName: header.SiteName,
+                    DbOrPanelNumber: header.DbOrPanelNumber, 
+                    HeaderTestDate: header.TestDate, 
+                    SheetNumber: header.SheetNumber, 
+                    AibRef: header.AibRef,
+                    TpOrSp: header.TpOrSp,
+                    Location: header.Location, 
+                    DbOrPanelIncomerDetails: header.DbOrPanelIncomerDetails,
+                    CableNum: cableNum, 
+                    FedFrom: fedFrom, 
+                    CircuitRefAndPhase: circuitRefAndPhase,
+                    CircuitDescription: sheet.Cell(13, col).GetString(),
+                    CircuitType: sheet.Cell(14, col).GetString(),
+                    CableType: sheet.Cell(15, col).GetString(),
+                    InstallationMethod: sheet.Cell(16, col).GetString(), 
+                    CableLength: sheet.Cell(17, col).GetString(),
+                    NumOfCoresCSA: sheet.Cell(18, col).GetString(),
+                    CircuitBreakerOrFuseRating: sheet.Cell(26, col).GetString(),
+                    CircuitBreakerBSAndTypeNum: sheet.Cell(27, col).GetString(),
+                    CircuitBreakerManufacturerAndRefNum: sheet.Cell(28, col).GetString(),
+                    RCDManufacturerAndType: sheet.Cell(31, col).GetString(),
+                    Load: sheet.Cell(34, col).GetString(),
+                    RatingKW: sheet.Cell(35, col).GetString(),
+                    FullLoadCurrentA: sheet.Cell(36, col).GetString(),
+                    CircuitVoltageV: sheet.Cell(58, col).GetString(),
+                    CircuitCurrentA: sheet.Cell(59, col).GetString(),
+                    TestDate: sheet.Cell(60, col).GetString(),
+                    Comments: sheet.Cell(61, col).GetString()
+                );
+            }
+        }
+
+    }
+}
+
